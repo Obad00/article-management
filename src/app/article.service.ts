011d81636ff5ad325fa142b3a89c3a45a3e420d9
Adama Dabo <dabodabo9064@gmail.com>
2024-07-31 15:25:35 +0000

    Test

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,3 +1,4 @@
+// src/app/article.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -6,31 +7,27 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ArticleService {
-  private apiUrl = 'https://jsonplaceholder.typicode.com'; // Base URL de votre API
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
 
   constructor(private http: HttpClient) { }
 
   getArticles(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/posts`);
+    return this.http.get<any[]>(this.apiUrl);
   }
 
   getArticle(id: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/posts/${id}`);
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
   }
-  
+
   createArticle(article: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/posts`, article);
+    return this.http.post<any>(this.apiUrl, article);
   }
 
   updateArticle(id: number, article: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/posts/${id}`, article);
+    return this.http.put<any>(`${this.apiUrl}/${id}`, article);
   }
 
   deleteArticle(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/posts/${id}`);
-  }
-
-  getCommentsForArticle(id: number): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/posts/${id}/comments`);
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
   }
 }
